43ece5ac359d7d63d2b0d50d68a36a9275a2c013
Mayuresh Palmate <mayureshpalmate.mp@gmail.com>
2026-08-11 22:33:01 +0530

    Add files via upload

---
+++ Day1.py
@@ -0,0 +1,5 @@
+name = 'Mayuresh'
+field = 'Cybersecurity'
+
+print(f'My name is {name}.')
+print(f'I want to become a {field} professional.')
---
+++ Day2.py
@@ -0,0 +1,9 @@
+name = input('Enter your name: ')
+age = int(input('Enter your age: '))
+
+next_age = age + 1
+
+print(f'Hello {name}!')
+print(f'You are {age} years old.')
+print(f'Next year you will be {next_age} years old.')
+print(f'In 5 years you will be {age + 5} years old.')
